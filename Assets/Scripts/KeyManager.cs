335562829b9e54d9a8c650285d0c652eb6b6b1b3
Kevin Xue <ivexnuke@gmail.com>
2017-08-05 11:12:47 -0400

    setup backgrounds and most required prefabs.

---
+++ Assets/Scripts/KeyManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyManager : MonoBehaviour {
+
+	public string m_Key;
+	public float m_NoteTravelTime;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown(m_Key)){
+			//play music and stuff here. Also maybe launch missile
+		}
+	}
+}
